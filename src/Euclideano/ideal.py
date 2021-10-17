2231a05adff022b07565f298815ea92e02a97ff2
Eduardo Lara Ramírez <54120988+eduardo-lr@users.noreply.github.com>
2021-10-17 11:55:49 -0500

    Update ideal.py

--- src/Euclideano/ideal.py
+++ src/Euclideano/ideal.py
@@ -9,21 +9,21 @@ h = 2*np.pi
 
 # Funcion de particion de una particula.
 def z (V,T):
-    I = integrate.quad(lambda x: np.exp(-((1/(k*T))*(x**2)/(2*m))), -np.inf, np.inf)
-    return np.power(I[0]/h,3)*V
+	I = integrate.quad(lambda x: np.exp(-((1/(k*T))*(x**2)/(2*m))), -np.inf, np.inf)
+	return np.power(I[0]/h,3)*V
 
 # logaritmo de la funcion de particion de N particulas.
 def lnZ (T, N, V):
-    # Usamos la aproximacion de Stirling.
-    return N + N*np.log(z(V,T)) - N*np.log(N)
+	# Usamos la aproximacion de Stirling.
+	return N + N*np.log(z(V,T)) - N*np.log(N)
 
 # Energia interna del sistema.
 def E (T, N, V):
-    return k*(T**2)*derivative(lambda x: lnZ(x, N, V), T, dx=1e-5)
+	return k*(T**2)*derivative(lambda x: lnZ(x, N, V), T, dx=1e-5)
 
 # Presion
 def p (T, N, V):
-    return k*T*N/z(V,T)*derivative(lambda x: z(x, T), V, dx=1e-5)
+	return k*T*N/z(V,T)*derivative(lambda x: z(x, T), V, dx=1e-5)
 
 def graficarVsT (f):
 	Tmaxima = 1000
